2573e830c25cc355ade8c2542e89dbacff567428
budavarik <budavarik@gmail.com>
2017-03-13 20:11:05 +0100

    first

---
+++ src/test/java/com/selenium/tests/LoginPage.java
@@ -0,0 +1,10 @@
+package com.selenium.tests;
+
+import org.openqa.selenium.By; 
+
+public class LoginPage {
+
+    public static final By INPUT_FIELD_LOGON_ID = By.name("logonIdX");
+    public static final By INPUT_FIELD_PASSWORD = By.name("password");
+    public static final By INPUT_BUTTON_LOGIN = By.id("submitLogin"); 
+}
